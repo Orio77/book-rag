89d627165120e7e97d569520a634c9f04887313d
Orio <maciejll@wp.pl>
2025-11-07 14:35:36 +0100

    0.1 Initialize Repository

---
+++ src/main/java/com/orio77/book_rag/BookRagApplication.java
@@ -0,0 +1,15 @@
+package com.orio77.book_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRagApplication.class, args);
+
+		System.out.println("Hello BookRagApplication");
+	}
+
+}
---
+++ src/test/java/com/orio77/book_rag/BookRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orio77.book_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
